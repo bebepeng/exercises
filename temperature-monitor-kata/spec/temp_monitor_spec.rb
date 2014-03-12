4464e473c8f9b2f4f33ae07604401967e42a54db
Bebe Peng <bebepeng@gmail.com>
2014-03-12 10:10:16 -0600

    test checks the data was imported as an array

--- temperature-monitor-kata/spec/temp_monitor_spec.rb
+++ temperature-monitor-kata/spec/temp_monitor_spec.rb
@@ -2,11 +2,11 @@ require 'rspec/core'
 require 'temp_monitor'
 
 describe TempMonitor do
-  it "reads a file" do
+  it "reads a file into an array" do
     tm = TempMonitor.new
     tm.read_data("../weather_data.txt")
-    expect(tm.data).not_to eq(nil)
     expect(tm.data.length).not_to eq(0)
+    expect(tm.data.is_a? Array).to eq true
   end
   it "finds the difference between highs and lows for one day"
   it "compares the differences of different days"
