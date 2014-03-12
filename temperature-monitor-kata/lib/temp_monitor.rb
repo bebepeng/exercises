f7ca4bc26a08fba5e44a8f56891bdd69c2e0d86d
Bebe Peng <bebepeng@gmail.com>
2014-03-12 11:04:38 -0600

    calculates the temperature spread for a certain day

--- temperature-monitor-kata/lib/temp_monitor.rb
+++ temperature-monitor-kata/lib/temp_monitor.rb
@@ -6,6 +6,15 @@ class TempMonitor
 
   def read_data(file)
     @data = File.open("weather_data.txt").read
-    @data = @data.each_line.to_a
+    @data = @data.lines
+    @data = @data.slice(8..-3)
+    @data.map! do |day|
+       day.split(" ")
+    end
+  end
+
+  def temp_spread(day)
+    i = day - 1
+    @data[i][1].to_i - @data[i][2].to_i
   end
 end
--- temperature-monitor-kata/spec/temp_monitor_spec.rb
+++ temperature-monitor-kata/spec/temp_monitor_spec.rb
@@ -10,7 +10,9 @@ describe TempMonitor do
     expect(@tm.data.length).not_to eq(0)
     expect(@tm.data.is_a? Array).to eq true
   end
-  it "finds the difference between highs and lows for one day"
+  it "finds the temperature spread for one day" do
+    expect(@tm.temp_spread(1)).to eq 29
+  end
   it "compares the differences of different days"
   it "tells me what is the smallest temperature spread witht the associated day"
 end
